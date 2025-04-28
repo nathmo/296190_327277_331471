f01f1fed99b5994c076ff0caa1ff6e166a86fb26
Nathann Morand <natmo@hotmail.ch>
2025-04-28 10:52:26 +0200

    Update yoco.py
    YOCO is 4M parameters
    YOCO large is hopefully not over 12M

--- project/src/yoco.py
+++ project/src/yoco.py
@@ -47,47 +47,6 @@ class YOCO(nn.Module):
         self.output_dim = num_classes * count_range
 
         self.stem = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(32),
-            nn.PReLU(32),
-        )
-
-        self.layer1 = BasicBlock(32, 64, downsample=True)    # 600x400
-        self.layer2 = BasicBlock(64, 128, downsample=True)   # 300x200
-        self.layer3 = BasicBlock(128, 256, downsample=True)  # 150x100
-        self.layer4 = BasicBlock(256, 512, downsample=True)  # 75x50
-        self.layer5 = BasicBlock(512, 512, downsample=True)  # 37x25
-        self.layer6 = BasicBlock(512, 512, downsample=True)  # 18x12
-
-        self.head = nn.Sequential(
-            nn.Conv2d(512, 256, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(256),
-            nn.PReLU(256),
-
-            nn.Conv2d(256, self.output_dim, kernel_size=1, stride=1, bias=True),
-        )
-
-    def forward(self, x):
-        x = self.stem(x)
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-        x = self.layer5(x)
-        x = self.layer6(x)
-        x = self.head(x)
-        x = F.adaptive_avg_pool2d(x, (1, 1))
-        x = x.view(x.size(0), self.num_classes, self.count_range)
-        return x
-
-
-class YOCOLARGE(nn.Module):
-    def __init__(self, num_classes=13, count_range=6):
-        super(YOCOLARGE, self).__init__()
-        self.num_classes = num_classes
-        self.count_range = count_range
-        self.output_dim = num_classes * count_range
-        self.stem = nn.Sequential(
             nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(16),
             nn.PReLU(16),
@@ -119,6 +78,49 @@ class YOCOLARGE(nn.Module):
         x = F.adaptive_avg_pool2d(x, (1, 1))
         x = x.view(x.size(0), self.num_classes, self.count_range)
         return x
+
+
+class YOCOLARGE(nn.Module):
+    def __init__(self, num_classes=13, count_range=6):
+        super(YOCOLARGE, self).__init__()
+        self.num_classes = num_classes
+        self.count_range = count_range
+        self.output_dim = num_classes * count_range
+
+        self.stem = nn.Sequential(
+            nn.Conv2d(3, 24, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(24),
+            nn.PReLU(24),
+        )
+
+        self.layer1 = BasicBlock(24, 48, downsample=True)    # 600x400
+        self.layer2 = BasicBlock(48, 96, downsample=True)    # 300x200
+        self.layer3 = BasicBlock(96, 192, downsample=True)   # 150x100
+        self.layer4 = BasicBlock(192, 256, downsample=True)  # 75x50
+        self.layer5 = BasicBlock(256, 256, downsample=True)  # 37x25
+        self.layer6 = BasicBlock(256, 256, downsample=True)  # 18x12
+
+        self.head = nn.Sequential(
+            nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.PReLU(128),
+
+            nn.Conv2d(128, self.output_dim, kernel_size=1, stride=1, bias=True),
+        )
+
+    def forward(self, x):
+        x = self.stem(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.layer5(x)
+        x = self.layer6(x)
+        x = self.head(x)
+        x = F.adaptive_avg_pool2d(x, (1, 1))
+        x = x.view(x.size(0), self.num_classes, self.count_range)
+        return x
+
 
 
 class YOCOSMALL(nn.Module):
